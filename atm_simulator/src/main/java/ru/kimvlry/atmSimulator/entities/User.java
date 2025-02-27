37642e9ebd26418904128ccc93d4fe655422ef61
kimvlry <412963@niuitmo.ru>
2025-02-27 11:53:14 +0300

    feat: add documentation

--- atm_simulator/src/main/java/ru/kimvlry/atmSimulator/entities/User.java
+++ atm_simulator/src/main/java/ru/kimvlry/atmSimulator/entities/User.java
@@ -12,22 +12,24 @@ import java.util.Map;
 import java.util.UUID;
 
 /**
- *
+ * Represents a User in the ATM system.
+ * Stores all the User's bank accounts and manages operations on the currently selected account.
  */
 public class User {
     private Map<String, BankAccount> accounts;
     private String currentAccountID;
 
     /**
-     *
+     * Creates a new User with an empty collection of bank accounts.
      */
     public User() {
         this.accounts = new HashMap<>();
     }
 
     /**
+     * Creates a new bank account for the User.
      *
-     * @return
+     * @return a unique identifier assigned to the newly created account
      */
     public String createAccount() {
         String accountNumber = UUID.randomUUID().toString();
@@ -39,9 +41,10 @@ public class User {
     }
 
     /**
+     * Switches the current account to another by changing {@code currentAccountID} value
      *
-     * @param accountNumber
-     * @throws AccountNotFoundException
+     * @param accountNumber the identifier of the account to switch to
+     * @throws AccountNotFoundException if the specified ID is not associated with any of User's accounts
      */
     public void switchAccount(String accountNumber) throws AccountNotFoundException {
         if (!accounts.containsKey(accountNumber)) {
@@ -51,9 +54,10 @@ public class User {
     }
 
     /**
+     * Returns the balance of the currently selected account by calling the corresponding {@link BankAccount} method.
      *
-     * @return
-     * @throws NoAccountSelectedException
+     * @return the balance of the active account
+     * @throws NoAccountSelectedException if no account is currently selected
      */
     public BigDecimal getBalance() throws NoAccountSelectedException {
         if (currentAccountID == null) {
@@ -63,10 +67,12 @@ public class User {
     }
 
     /**
+     * Deposits a specified amount into the currently selected account by calling the corresponding {@link BankAccount} method.
      *
-     * @param amount
-     * @return
-     * @throws NoAccountSelectedException
+     * @param amount the amount to deposit
+     * @return the updated balance after the deposit
+     * @throws NoAccountSelectedException if no account is currently selected
+     * @throws InvaidAmountException if the provided amount is invalid (e.g., non-positive)
      */
     public BigDecimal deposit(BigDecimal amount) throws NoAccountSelectedException, InvaidAmountException {
         if (currentAccountID == null) {
@@ -76,11 +82,13 @@ public class User {
     }
 
     /**
+     * Withdraws a specified amount from the currently selected account by calling the corresponding {@link BankAccount} method.
      *
-     * @param amount
-     * @return
-     * @throws NoAccountSelectedException
-     * @throws InsufficientFundsException
+     * @param amount the amount to withdraw
+     * @return the updated balance after the withdrawal
+     * @throws NoAccountSelectedException if no account is currently selected
+     * @throws InsufficientFundsException if the account has insufficient funds
+     * @throws InvaidAmountException if the provided amount is invalid (e.g., non-positive)
      */
     public BigDecimal withdraw(BigDecimal amount) throws NoAccountSelectedException, InsufficientFundsException, InvaidAmountException {
         if (currentAccountID == null) {
@@ -90,9 +98,10 @@ public class User {
     }
 
     /**
+     * Returns the transaction history of the currently selected account by calling the corresponding {@link BankAccount} method.
      *
-     * @return
-     * @throws NoAccountSelectedException
+     * @return a list of transaction records for the active account
+     * @throws NoAccountSelectedException if no account is currently selected
      */
     public List<String> getTransactionHistory() throws NoAccountSelectedException {
         if (currentAccountID == null) {
