3ae93c14f54846b68eae84bd08051cf1f93f5aa9
kimvlry <412963@niuitmo.ru>
2025-04-09 18:14:48 +0300

    feat: demo run

--- kittens/src/main/java/ru/kimvlry/kittens/Main.java
+++ kittens/src/main/java/ru/kimvlry/kittens/Main.java
@@ -1,8 +1,14 @@
 package ru.kimvlry.kittens;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import org.flywaydb.core.Flyway;
 import ru.kimvlry.kittens.dao.KittenDao;
 import ru.kimvlry.kittens.dao.OwnerDao;
-import ru.kimvlry.kittens.entities.*;
+import ru.kimvlry.kittens.entities.Kitten;
+import ru.kimvlry.kittens.entities.KittenBreed;
+import ru.kimvlry.kittens.entities.Owner;
 import ru.kimvlry.kittens.exceptions.DatabaseException;
 import ru.kimvlry.kittens.exceptions.InvalidInstanceException;
 import ru.kimvlry.kittens.services.KittenService;
@@ -10,21 +16,20 @@ import ru.kimvlry.kittens.services.OwnerService;
 
 import java.time.LocalDateTime;
 import java.util.Set;
-import org.flywaydb.core.Flyway;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
-
 
 public class Main {
     public static void main(String[] args) throws InvalidInstanceException, DatabaseException {
+
         String jdbcUrl = "jdbc:postgresql://localhost:5432/kittens_demo";
         String username = "postgres";
         String password = "postgres";
 
         Flyway flyway = Flyway.configure()
                 .dataSource(jdbcUrl, username, password)
+                .cleanDisabled(false)
                 .load();
+
+        flyway.clean();
         flyway.migrate();
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("kittens_demo");
@@ -34,58 +39,42 @@ public class Main {
         KittenService kittenService = new KittenService(new KittenDao(em));
 
         try {
-            demoCRUD(ownerService, kittenService);
-            demoFriendship(kittenService);
+            Owner alice = new Owner();
+            alice.setName("Alice");
+            alice.setBirthTimestamp(LocalDateTime.now());
+
+            Owner bob = new Owner();
+            bob.setName("Bob");
+            bob.setBirthTimestamp(LocalDateTime.now());
+
+            Kitten whiskers = new Kitten();
+            whiskers.setName("Whiskers");
+            whiskers.setBirthTimestamp(LocalDateTime.now());
+            whiskers.setBreed(KittenBreed.SIBERIAN);
+            whiskers.setOwner(alice);
+            alice.setOwnedKittens(Set.of(whiskers));
+
+            Kitten muffin = new Kitten();
+            muffin.setName("Muffin");
+            muffin.setBirthTimestamp(LocalDateTime.now());
+            muffin.setBreed(KittenBreed.MAINE_COON);
+            muffin.setOwner(bob);
+            bob.setOwnedKittens(Set.of(muffin));
+
+            ownerService.addOwner(alice);
+            ownerService.addOwner(bob);
+
+            whiskers.getFriends().add(muffin);
+            muffin.getFriends().add(whiskers);
+            kittenService.update(whiskers);
+            kittenService.update(muffin);
+
+            Kitten fetched = kittenService.getById(whiskers.getId());
+            System.out.println(fetched.getName() + " has " + fetched.getFriends().size() + " friends.");
+
         } finally {
             em.close();
             emf.close();
         }
-    }
-
-    private static void demoCRUD(OwnerService ownerService, KittenService kittenService) throws InvalidInstanceException, DatabaseException {
-        System.out.println("\nCRUD");
-
-        Owner owner = new Owner();
-        owner.setName("Owner");
-        owner.setBirthTimestamp(LocalDateTime.now());
-
-        Kitten kitten = new Kitten();
-        kitten.setName("Kitty");
-        kitten.setBirthTimestamp(LocalDateTime.now());
-        kitten.setBreed(KittenBreed.SIBERIAN);
-
-        owner.setOwnedKittens(Set.of(kitten));
-        kitten.setOwner(owner);
-
-        Owner savedOwner = ownerService.addOwner(owner);
-        System.out.println("Saved owner: " + savedOwner.getName() + " with " +
-                savedOwner.getOwnedKittens().size() + " kittens");
-
-        savedOwner.setName("Cooler Owner");
-        ownerService.update(savedOwner);
-        System.out.println("Updated owner name: " + ownerService.getById(savedOwner.getId()).getName());
-    }
-
-    private static void demoFriendship(KittenService kittenService) throws InvalidInstanceException, DatabaseException {
-        System.out.println("\nfriendship");
-
-        Kitten cat1 = new Kitten();
-        cat1.setName("Kit");
-        cat1.setBirthTimestamp(LocalDateTime.now());
-        cat1.setBreed(KittenBreed.BRITISH_SHORTHAIR);
-
-        Kitten cat2 = new Kitten();
-        cat2.setName("Kat");
-        cat2.setBirthTimestamp(LocalDateTime.now());
-        cat2.setBreed(KittenBreed.MAINE_COON);
-
-        cat1.getFriends().add(cat2);
-        cat2.getFriends().add(cat1);
-
-        Kitten savedCat1 = kittenService.addKitten(cat1);
-        Kitten savedCat2 = kittenService.addKitten(cat2);
-
-        System.out.println(savedCat1.getName() + " has " +
-                savedCat1.getFriends().size() + " friends");
     }
 }
