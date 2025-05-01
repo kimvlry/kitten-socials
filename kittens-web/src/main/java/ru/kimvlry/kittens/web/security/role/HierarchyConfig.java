257dbf93be332be6af2eee713dba091c071e2b5c
kimvlry <412963@niuitmo.ru>
2025-05-01 15:24:53 +0300

    feat: user interfaces impl + roles hierarchy

---
+++ kittens-web/src/main/java/ru/kimvlry/kittens/web/security/role/HierarchyConfig.java
@@ -0,0 +1,17 @@
+package ru.kimvlry.kittens.web.security.role;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+
+@Configuration
+public class HierarchyConfig {
+
+    @Bean
+    public RoleHierarchy Hierarchy() {
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
+        return roleHierarchy;
+    }
+}
---
+++ kittens-web/src/main/java/ru/kimvlry/kittens/web/security/user/UserDetailsImpl.java
@@ -0,0 +1,79 @@
+package ru.kimvlry.kittens.web.security.user;
+
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import ru.kimvlry.kittens.entities.Owner;
+import ru.kimvlry.kittens.web.security.role.Role;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Entity
+@Table(name = "users")
+public class UserDetailsImpl implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToOne()
+    @JoinColumn(name = "owner_id",
+                referencedColumnName = "id",
+                unique = true)
+    private Owner owner;
+
+    @ManyToMany()
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
